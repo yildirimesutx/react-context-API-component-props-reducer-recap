004d2cb2b4921e9759ac4393e43199156cdc1721
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-07-21 15:35:11 +0300

    components and props

---
+++ components-props/README.md
@@ -0,0 +1,5 @@
+# Components and Props
+
+<img href="./../notes/components-props-1.jpeg">
+<img href="./../notes/components-props-2.jpeg">
+
---
+++ components-props/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Components Notes  <NameView name_1="Mete"/></h1>
+     <Hello name="Mesut">
+      <img src="https://picsum.photos/200/300" alt="" />
+      <p>ramdom image, props children</p>
+
+     </Hello>
+     <p>onay için <NewButon buton="Enter"/></p>
+     <NewButon buton="Enter"/>
+    </div>
+  );
+}
+
+export default App;
+
+function Hello(props){
+  console.log(props);
+  return (<div>
+    <p>Merhaba, <NameView name_1={props.name}/> </p> 
+     <p>{props.children}</p>
+    
+    </div>
+    );
+}
+
+function NameView(props){
+   console.log(props)
+  return <a href="/"> {props.name_1 || "misafir" }</a>
+}
+
+
+function NewButon(props){
+
+  return <button>{props.buton}</button>
+}
+
+
+
